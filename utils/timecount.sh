d05dc4af55c4f8f7c8425e5364e3e7ebaac15c66
c_thang.pham <thang.pham@terralogic.com>
2018-08-15 09:53:04 +0700

    fixed bugs

--- utils/timecount.sh
+++ utils/timecount.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #read ip from iplist and ping to check it's alive or not
-ipfile="/tmp/iplist"
-macfile="/tmp/maclist.txt"
+ipfile="/home/thangpham/utils/iplist"
+macfile="/home/thangpham/utils/maclist.txt"
 loop=1
 ipaddr=localhost
 init_text="working_minutes
-0
+960
 working_hrs
-0"
+16"
 reset_time=0
 function monday_check(){
   dayofweek=`date +"%w"`
@@ -36,11 +36,7 @@ function check_all_ips(){
         echo "File not found!"
         touch "$filename"
         echo "$init_text" >  "$filename"
-        # echo "working_minutes" > $filename
-        # echo "0" > $filename
-        # echo "working_hrs" > $filename
-        # echo "0" > $filename
-
+    
       fi
       #set ip before check
       ip=$var
@@ -53,13 +49,12 @@ function check_all_mac(){
   while IFS= read -r mac 
     do
       echo "check mac addr $mac"
-      filename="/tmp/${mac}.txt"
+      filename="/home/thangpham/utils/${mac}.txt"
       echo "filename=$filename"
       #create file with ip from iplist if it wasnt created yet
       echo "reset_time=$reset_time"
       if [ ! -f "$filename" ] || [ "$reset_time" = 1 ]; then
         echo "File not found!"
-        touch "$filename"
         echo "$init_text" >  "$filename"
       
       fi
@@ -75,29 +70,31 @@ function check_alive(){
 
     ping -c 1 $ipaddr                      # Try once.
     rc=$?
-    filename="/tmp/${mac}.txt"
-    echo "file name contain timesheet is $filename"
+    tmp_filename="/tmp/${mac}.txt"
+    store_filename="/home/thangpham/utils/${mac}.txt"
+    echo "file name contain timesheet is $tmp_filename and $store_filename"
     if [[ $rc -eq 0 ]] ; then
         #read current minutes from the timesheet file
         #read  2nd line
-         working_minutes=$(sed -n '2p' "$filename")
-          #$(awk 'NR==2 ' "$filename") 
+         working_minutes=$(sed -n '2p' "$store_filename")
          echo "working_minutes=$working_minutes" 
 
  	       working_minutes=$((working_minutes+10))
          echo "working_minutes=$working_minutes" 
 	       working_hrs=$((working_minutes/60))
 	       echo "working_hrs=$working_hrs"
-	       # echo "working_times " > filename
-        #  echo "$working_minutes" >> filename
-        #  echo "working_hrs " >> filename
-        #  echo "$working_hrs" >> filename
+	       #store temporary file
         echo "working_minutes
 $working_minutes
 working_hrs
-$working_hrs" > $filename
+$working_hrs" > $tmp_filename
+ #store to permanent file
+        echo "working_minutes
+$working_minutes
+working_hrs
+$working_hrs" > $store_filename
+
     fi;
-    #sleep 60
 }
 
 
@@ -108,5 +105,5 @@ do
         #check_all_ips
         check_all_mac
         echo "sleep for 10minutes"
-        sleep 1m
+        sleep 10m
 done
